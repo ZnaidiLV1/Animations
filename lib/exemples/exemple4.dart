271f70986ca9e6ae70c3e5b9c7ce99c0dda08e57
znaidi <znaidi2049@gmail.com>
2024-07-09 10:14:23 +0100

    Adding the animation using Hero widget

--- lib/exemples/exemple4.dart
+++ lib/exemples/exemple4.dart
@@ -1,3 +1,4 @@
+import 'package:animation/exemples/exemple4Detail.dart';
 import 'package:flutter/material.dart';
 
 class Personne {
@@ -8,12 +9,10 @@ class Personne {
   Personne({required this.emoji, required this.name, required this.age});
 }
 
-
-
 List<Personne> personne = [
   Personne(emoji: '🙋‍♂️', name: "znaidi", age: 22),
   Personne(emoji: '🤦‍♂️', name: "oussema", age: 23),
-  Personne(emoji: '😊', name: "happy", age: 22),
+  Personne(emoji: '😊', name: "wissem", age: 22),
 ];
 
 class exemple4 extends StatefulWidget {
@@ -29,14 +28,21 @@ class _exemple4State extends State<exemple4> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xff424242),
-        title: Text("People"),
+        title: Text("People",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
         centerTitle: true,
       ),
       body: ListView.builder(
           itemCount: personne.length,
           itemBuilder: (context, index) {
             return ListTile(
-              title: Text(personne[index].name),
+              onTap: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        PersonDetail(person: personne[index])));
+              },
+              title: Hero(
+                  tag: personne[index].name, child: Text(personne[index].name)),
               subtitle: Text(personne[index].age.toString()),
               leading: Text(
                 personne[index].emoji,
--- lib/exemples/exemple4Detail.dart
+++ lib/exemples/exemple4Detail.dart
@@ -3,8 +3,7 @@ import 'package:flutter/material.dart';
 
 class PersonDetail extends StatefulWidget {
   final Personne person;
-  const PersonDetail({super.key, required
-  this.person});
+  const PersonDetail({super.key, required this.person});
 
   @override
   State<PersonDetail> createState() => _PersonDetailState();
@@ -16,16 +15,52 @@ class _PersonDetailState extends State<PersonDetail> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xff424242),
-        title: Text("People"),
+        title: Hero(
+          flightShuttleBuilder: (
+            flightContext,
+            animation,
+            flightDirection,
+            fromHeroContext,
+            toHeroContext,
+          ) {
+            switch (flightDirection) {
+              case HeroFlightDirection.push:
+                return Material(
+                  color: Colors.transparent,
+                  child: ScaleTransition(
+                      scale: animation.drive(Tween(begin: 0.0, end: 1.0)
+                          .chain(CurveTween(curve: Curves.fastOutSlowIn))),
+                      child: toHeroContext.widget),
+                );
+              case HeroFlightDirection.pop:
+                return Material(
+                  color: Colors.transparent,
+                  child: fromHeroContext.widget,
+                );
+            }
+          },
+          tag: widget.person.name,
+          child: Material(
+            color: Colors.transparent,
+            child: Text(widget.person.emoji,
+                style: TextStyle(fontSize: 38, fontWeight: FontWeight.bold)),
+          ),
+        ),
         centerTitle: true,
       ),
       body: Column(
         children: [
           Container(
-            child: Text(widget.person.name),
+            margin: EdgeInsets.all(15),
+            alignment: Alignment.center,
+            child: Text(
+              widget.person.name,
+              style: TextStyle(fontSize: 25),
+            ),
           ),
           Container(
-            child: Text(widget.person.age.toString()),
+            child: Text(widget.person.age.toString(),
+                style: TextStyle(fontSize: 25)),
           ),
         ],
       ),
